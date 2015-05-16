c927189f0a688d27dbe8a2c6d10ae92c518dc9ae
michael-eneji <bigmikeeneji@gmail.com>
2015-05-16 11:15:15 +0100

    MY C++ASSIGNMENT

---
+++ mike test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main ()
+{
+	int greeting;
+	cout << "please press enter: ";
+	cout << "welcome and God bless you: ";
+	
+	return 0;
+}
+
